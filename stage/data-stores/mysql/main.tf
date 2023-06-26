ebbf639509da4d859112d043e8b9319b0e7fd130
Steve <surrylic@me.com>
2023-06-26 06:33:25 -0400

    Can I connect to github??

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -8,8 +8,29 @@ resource "aws_db_instance" "example" {
     allocated_storage = 10
     instance_class = "db.t2.micro"
     skip_final_snapshot = true
-    db_name = "example_database"    
+    db_name = "example_database"
+
+    username = var.db_username
+    password = var.db_password
 }
 
-username = "???"
-password = "???"
+terraform {
+  backend "s3" {
+    bucket = "062023-terraform-state-bucket"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+  dynamodb_table = "terraform-up-and-running-locks"
+  encrypt = true
+  }
+}
+
+output "address" {
+    value = aws_db_instance.example.address
+    description = "Database IP Address"
+}
+
+output "port" {
+    value = aws_db_instance.example.port
+    description = "Database port"
+}
